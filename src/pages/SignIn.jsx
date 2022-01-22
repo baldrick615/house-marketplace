bc0448293d9c52c17050cc8764c730371fbbcdde
Tim McGrath <tmcgrath28@gmail.com>
2022-01-22 12:11:23 -0600

    building signIn page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,18 @@
+import { useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibityIcon from '../assets/svg/visibilityIcon.svg'
+
 function SignIn() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    setShowPassword: ''
+  })
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+
   return (
     <div>
       <h1>Sign In</h1>
