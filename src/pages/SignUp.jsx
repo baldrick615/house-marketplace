089037728ca5a6e41383069d055ab01ab1f6d26c
Tim McGrath <tmcgrath28@gmail.com>
2022-01-23 15:14:35 -0600

    working on OAuth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,9 @@ import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibityIcon from '../assets/svg/visibilityIcon.svg'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import { db } from '../firebase/config'
+import {logDOM} from '@testing-library/react'
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -21,6 +24,24 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e)=> {
+    e.preventDefault()
+    try{
+      const auth = getAuth()
+      const userCredential = await createUserWithEmailAndPassword
+      (auth, email, password)
+
+      const user = userCredential.user
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+      navigate('/')
+    } catch (error){
+      console.log(error)
+    }
+
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -28,7 +49,7 @@ function SignUp() {
           <p className="pageHeader">Welcome to the Site!</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input type="text"
                  className="nameInput"
                  placeholder="Name"
